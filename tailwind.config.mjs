151dfc4d43c6c4eb05ce4295eff025d2ebdefa7e
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-01-31 16:39:26 -0300

    add tailwind + add react + configure astro server + add prettier

---
+++ tailwind.config.mjs
@@ -0,0 +1,40 @@
+import defaultTailwindColors from 'tailwindcss/colors'
+
+/* As of Tailwind CSS v2.2, certain colors has been renamed  */
+const deprecatedColors = [
+  'lightBlue',
+  'warmGray',
+  'coolGray',
+  'blueGray',
+  'trueGray'
+]
+
+for (const color of deprecatedColors) {
+  delete defaultTailwindColors[color]
+}
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+    colors: {
+      // dark mode
+      'soft-white': '#e8ecfb',
+      midnight: '#040718',
+      primary: '#5020cb',
+      secondary: '#2c168c',
+      accent: '#6232df',
+
+      // light mode
+      'soft-dark': '#040816',
+      noon: '#e9ecfb',
+      'light-primary': '#142480',
+      'light-secondary': '#8872e9',
+      'light-accent': '#5020cb',
+
+      ...defaultTailwindColors
+    }
+  },
+  plugins: []
+}
