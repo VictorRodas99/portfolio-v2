2494342064adc7496efe65f0abe2675c3dfc8b64
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-02-02 19:49:51 -0300

    modify to only have interfaces + using data in json

--- src/types/experience.d.ts
+++ src/types/experience.d.ts
@@ -5,27 +5,3 @@ export interface Experience {
   description: string
   link?: string
 }
-
-export const experiences = Object.freeze([
-  {
-    date: 'Actualmente...',
-    title: 'Desarrollador Front-End',
-    company: 'Binario Informática',
-    description:
-      'Responsable de la creación de utilidades y componentes para el desarrollo de aplicaciones web. Mejora significativa de la experiencia de usuario y el rendimiento de aplicaciones web.'
-  },
-  {
-    date: 'Noviembre 2023',
-    title: 'Analista de Sistemas, Desarrollador Front-End',
-    company: 'CODYS S.A',
-    description:
-      'Responsable de la planificación de proyectos. Identificación de problemas relacionados con cron-jobs en un sistema de alta demanda en España.'
-  },
-  {
-    date: 'Agosto 2022',
-    title: 'Desarrollador Front-End',
-    company: 'Freelance',
-    description:
-      'Principal desarrollador y diseñador de interfaces de usuario. Integración exitosa del sistema con visualización de datos dinámicos en tiempo real desde diferentes dispositivos.'
-  }
-])
