3de19f69136d2bbe6281c90cdf4d3afcb70ea081
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-02-02 19:52:19 -0300

    modify projects object + add function to get projects based on current language

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -10,89 +10,102 @@ import CloudflareIcon from '@/components/icons/cloudflare'
 import HonoIcon from '@/components/icons/hono'
 import CSSIcon from '@/components/icons/css'
 
-// change the color property in every project stack for colorClass
-const projects = Object.freeze([
-  {
-    name: 'Explora Ñeembucú – Descubre los encantos de Ñeembucú, Paraguay',
-    slug: 'explora-neembucu',
-    image: '/projects/explora-ñeembucu.webp',
-    description:
-      'Una página web desarrollada con Astro y React que recopila información sobre hoteles, restaurantes y puntos turísticos relevantes de diferentes zonas de Ñeembucú. Una guía de turismo única.',
-    github: 'https://github.com/VictorRodas99/explora-turismo-app',
-    demo: 'https://explora-turismo-app.vercel.app/',
-    stack: [
-      {
-        name: 'Astro',
-        colorClass: 'bg-[#060913] text-[#fff]',
-        icon: AstroIcon
-      },
-      {
-        name: 'Tailwind CSS',
-        colorClass: 'bg-[#003159] text-[#fff]',
-        icon: TailwindCSSIcon
-      },
-      {
-        name: 'React',
-        colorClass: 'bg-[#58c4dc] text-[#fff]',
-        icon: ReactIcon
-      },
-      {
-        name: 'Typescript',
-        colorClass: 'bg-[#3178C6] text-[#fff]',
-        icon: TypeScriptIcon
-      },
-      {
-        name: 'Supabase',
-        colorClass: 'bg-[#31b579] text-[#fff]',
-        icon: SupabaseIcon
-      }
-    ]
-  },
-  {
-    name: 'just-music – Una hermosa interfaz de línea de comandos',
-    slug: 'just-music',
-    image: '/projects/just-music.webp',
-    description:
-      'Una línea de comandos que permite reproducir música de YouTube sin necesidad de un navegador.',
-    github: 'https://github.com/VictorRodas99/just-music',
-    demo: 'https://www.npmjs.com/package/just-music',
-    stack: [
-      { name: 'Bash', colorClass: 'bg-[#000] text-[#fff]', icon: BashIcon },
-      {
-        name: 'PowerShell',
-        colorClass: 'bg-[#0072C6] text-[#fff]',
-        icon: PowershellIcon
-      },
-      {
-        name: 'JavaScript',
-        colorClass: 'bg-[#f0dc4e] text-[#fff]',
-        icon: JavaScriptIcon
-      }
-    ]
-  },
-  {
-    name: 'InfoShop - Ecommerce FullStack',
-    slug: 'info-shop',
-    image: '/projects/info-shop.webp',
-    description:
-      'Desarrollado con React y Hono. Esta aplicación te mantiene al día con los últimos productos de informática y electrónica. El catálogo se actualiza automáticamente de manera semanal.',
-    github: 'https://github.com/VictorRodas99/ecommerce-web-project',
-    demo: 'https://ecommerce-project-vik.netlify.app/',
-    stack: [
-      {
-        name: 'React',
-        colorClass: 'bg-[#58c4dc] text-[#fff]',
-        icon: ReactIcon
-      },
-      {
-        name: 'Edge Workers',
-        colorClass: 'bg-[#e36002] text-[#fff]',
-        icon: CloudflareIcon
-      },
-      { name: 'Hono', colorClass: 'bg-[#e36002] text-[#fff]', icon: HonoIcon },
-      { name: 'CSS', colorClass: 'bg-[#66309a] text-[#fff]', icon: CSSIcon }
-    ]
-  }
-])
+import spanishContent from '@/content/es/es.json'
+import englishContent from '@/content/en/en.json'
+import type { Language } from '@/supported-languages'
+import SUPPORTED_LANGUAGES from '@/supported-languages'
 
-export default projects
+const getProjects = (lang: Language) => {
+  if (!Object.values(SUPPORTED_LANGUAGES).includes(lang)) {
+    throw new Error('Unsupported language')
+  }
+
+  const projectsText =
+    lang === SUPPORTED_LANGUAGES.spanish
+      ? spanishContent.projects
+      : englishContent.projects
+
+  return [
+    {
+      ...projectsText['explora-neembucu'],
+      slug: 'explora-neembucu',
+      image: '/projects/explora-ñeembucu.webp',
+      github: 'https://github.com/VictorRodas99/explora-turismo-app',
+      demo: 'https://explora-turismo-app.vercel.app/',
+      stack: [
+        {
+          name: 'Astro',
+          colorClass: 'bg-[#060913] text-[#fff]',
+          icon: AstroIcon
+        },
+        {
+          name: 'Tailwind CSS',
+          colorClass: 'bg-[#003159] text-[#fff]',
+          icon: TailwindCSSIcon
+        },
+        {
+          name: 'React',
+          colorClass: 'bg-[#58c4dc] text-[#fff]',
+          icon: ReactIcon
+        },
+        {
+          name: 'Typescript',
+          colorClass: 'bg-[#3178C6] text-[#fff]',
+          icon: TypeScriptIcon
+        },
+        {
+          name: 'Supabase',
+          colorClass: 'bg-[#31b579] text-[#fff]',
+          icon: SupabaseIcon
+        }
+      ]
+    },
+    {
+      ...projectsText['just-music'],
+      slug: 'just-music',
+      image: '/projects/just-music.webp',
+      github: 'https://github.com/VictorRodas99/just-music',
+      demo: 'https://www.npmjs.com/package/just-music',
+      stack: [
+        { name: 'Bash', colorClass: 'bg-[#000] text-[#fff]', icon: BashIcon },
+        {
+          name: 'PowerShell',
+          colorClass: 'bg-[#0072C6] text-[#fff]',
+          icon: PowershellIcon
+        },
+        {
+          name: 'JavaScript',
+          colorClass: 'bg-[#f0dc4e] text-[#fff]',
+          icon: JavaScriptIcon
+        }
+      ]
+    },
+    {
+      ...projectsText['info-shop'],
+      slug: 'info-shop',
+      image: '/projects/info-shop.webp',
+      github: 'https://github.com/VictorRodas99/ecommerce-web-project',
+      demo: 'https://ecommerce-project-vik.netlify.app/',
+      stack: [
+        {
+          name: 'React',
+          colorClass: 'bg-[#58c4dc] text-[#fff]',
+          icon: ReactIcon
+        },
+        {
+          name: 'Edge Workers',
+          colorClass: 'bg-[#e36002] text-[#fff]',
+          icon: CloudflareIcon
+        },
+        {
+          name: 'Hono',
+          colorClass: 'bg-[#e36002] text-[#fff]',
+          icon: HonoIcon
+        },
+        { name: 'CSS', colorClass: 'bg-[#66309a] text-[#fff]', icon: CSSIcon }
+      ]
+    }
+  ]
+}
+
+export default getProjects
