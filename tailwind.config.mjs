9db28958b5c5d1a40c0dbb43b51c9c044ff9b5a5
VictorRodas99 <victormatiasrodas100@gmail.com>
2025-02-03 20:46:36 -0300

    configure darkMode to be selector-based

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,6 +16,7 @@ for (const color of deprecatedColors) {
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'selector',
   theme: {
     extend: {
       keyframes: {
